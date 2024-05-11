82acbf894467ea9310d2834456165fbd385ee312
Subin Dev <nguyenhuutri.me+1@gmail.com>
2024-05-11 13:05:45 +0700

    Update resetappstate.js

--- Commands/resetappstate.js
+++ Commands/resetappstate.js
@@ -1,2 +1,33 @@
 import { join } from "path";
 import { existsSync, writeFileSync, readFileSync } from "fs";
+const config = {
+    name: "resetappstate",
+    aliases: ["rsast"],
+    description: "Reset appstate",
+    usage: "[query]",
+    cooldown: 3,
+    permissions: [2],
+    credits: "Subin",
+    
+}
+
+async function onCall({ message }) {
+  try {
+    const { api } = global;
+    let ast = global.api.getAppState();
+    const data = JSON.stringify(ast);
+    const PATH = join(global.mainPath, 'appstate.json');
+    message.send(`Thành công reset appstate và reset server!`);
+    writeFileSync(PATH, data, "utf-8");
+    global.restart();
+  } catch (e) {
+    message.reply(`Lỗi:` + e);
+    console.log(e);
+  }
+}
+
+export default {
+  config,
+  onCall
+}
+
